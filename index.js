392e08a428f17640cb0cebd9a7a490d42e3a9d0d
Lindy Briggs <briggs.lindy@yahoo.com>
2022-04-20 17:07:44 -0500

    Added comments to index.js, updated README, added license.txt file.

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-// // TODO: Include packages needed for this application
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
 
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
-
+// Requiring modules.
 const fs = require('fs');
 const inquirer = require('inquirer')
 
+// Using inquirer to prompt user with array of messages in command line.
 inquirer
     .prompt([
         {
@@ -74,6 +78,8 @@ inquirer
             name: 'contributing',
         },
     ])
+// Using promise from .prompt to grab response values.
+// Using string interpolation to build body of md file. Grabbing values from response.
     .then((response) => {
         let content = `
 # ${response.projectTitle}
@@ -135,11 +141,11 @@ If you have additional questions, please reach out. Below are the best ways to c
 ${generateLicenseText(response)}
 
 `;
-
+// Using writeFile function from fs to generate the md file based on previously defined content..
         fs.writeFile('generatedREADME.md', content, (error) => console.error(error))
     })
 
-
+// Created functions to create text and badge based on license type user selected using if/then statements.
 function generateLicenseText(response) {
 
     if (response.license === "MIT") {
